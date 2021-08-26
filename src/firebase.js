56a43c4243420518f42f723e2bb0c0e8aad8b6fe
JinwooKim <jinwookim@leaprealm.com>
2021-08-26 14:03:06 +0900

    환경 설정하기

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA89fY2fjmzibwk8L8-bI4z6vPMl99ZqyY",
+    authDomain: "twitter-721b7.firebaseapp.com",
+    projectId: "twitter-721b7",
+    storageBucket: "twitter-721b7.appspot.com",
+    messagingSenderId: "39160299985",
+    appId: "1:39160299985:web:7d6a40a71829319cd920f0"
+};
+
+export default initializeApp(firebaseConfig);
