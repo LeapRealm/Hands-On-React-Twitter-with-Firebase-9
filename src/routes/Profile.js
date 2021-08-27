412b1c2594ad4b0456eaa4473f72235b6b90d77b
JinwooKim <jinwookim@leaprealm.com>
2021-08-27 20:27:28 +0900

    내비게이션 추가하고 로그아웃 처리하기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,21 @@
-const Profile = () => <span>Profile</span>;
+import { getAuth, signOut } from "firebase/auth";
+import { useHistory } from "react-router";
+
+const Profile = () => {
+  const auth = getAuth();
+  const history = useHistory();
+
+  const onLogOutClick = () => {
+    signOut(auth);
+    history.push("/");
+    // <Redirect from="*" to="/" />
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
