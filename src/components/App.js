9b092db80ca60f86b0c8ef11ac94de93775d3b9e
JinwooKim <jinwookim@leaprealm.com>
2021-08-27 19:45:39 +0900

    트위터에 로그인 기능 추가하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,40 @@
-import AppRouter from "./Router";
+import { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+initializeApp(firebaseConfig);
 
 function App() {
-  return <AppRouter />;
+  const auth = getAuth();
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Twitter</footer>
+    </>
+  );
 }
 
 export default App;
