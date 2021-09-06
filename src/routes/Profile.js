795b760237428b7e3243a45b05b7d0a66369b6f4
JinwooKim <jinwookim@leaprealm.com>
2021-09-06 22:32:54 +0900

    내가 쓴 트윗만 보기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,20 @@
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
 import { getAuth, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
+import Tweet from "components/Tweet";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const [tweets, setTweets] = useState([]);
+
+  const db = getFirestore();
   const auth = getAuth();
   const history = useHistory();
 
@@ -11,9 +24,34 @@ const Profile = () => {
     // <Redirect from="*" to="/" />
   };
 
+  useEffect(() => {
+    const q = query(
+      collection(db, "tweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "asc")
+    );
+    onSnapshot(q, (snapshot) => {
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setTweets(newArray);
+    });
+  }, [db, userObj.uid]);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
+
+      <div>
+        {tweets.map((tweet) => (
+          <Tweet
+            key={tweet.id}
+            tweetObj={tweet}
+            isOwner={tweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
     </>
   );
 };
